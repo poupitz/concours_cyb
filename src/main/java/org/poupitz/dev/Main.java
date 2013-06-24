274770693f4391345a5dc28d0fea9982be36dff0
poupitz <vinc.portal@gmail.com>
2013-06-24 11:46:06 +0200

    Dépot initial.

---
+++ src/main/java/org/poupitz/dev/Main.java
@@ -0,0 +1,14 @@
+package org.poupitz.dev;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Main 
+{
+    public static void main( String[] args ) throws FileNotFoundException
+    {
+    	BufferedReader r = new BufferedReader (new FileReader("data.properties"));
+        
+    }
+}
